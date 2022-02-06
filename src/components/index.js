d7fdd9900ba8efa2610b7845bcbbfb319c9f5b8e
Sunder Magar <sunder.magar@gmail.com>
2022-02-06 15:35:20 +0530

    new commit

--- src/components/index.js
+++ src/components/index.js
@@ -6,28 +6,50 @@ const SurpriseBox = () => {
     const [numberOfUsersVisted, setNumberOfUsersVisted] = useState([]);
     const [message, setMessage] = useState({});
 
-    const surpriseBoxwinningNumber = 2;
+    const surpriseBoxwinningNumber = [1, 2, 4, 8];
 
     const SubmituserName = () => {
-        if (!numberOfUsersVisted.includes(userName)) {
-            setNumberOfUsersVisted([...numberOfUsersVisted, userName]);
+        if (userName.trim().length <= 0) {
+            alert("please type user name");
+            return false;
+        }
+        const getUserPreviousResult = numberOfUsersVisted?.filter(
+            (item) => item.userName === userName
+        );
+        if (getUserPreviousResult.length > 0) {
+            if (
+                surpriseBoxwinningNumber.includes(
+                    getUserPreviousResult[0].userCount
+                )
+            ) {
+                showLooserOrWinner(true, userName);
+            } else {
+                showLooserOrWinner(false, userName);
+            }
+        } else {
+            if (surpriseBoxwinningNumber.includes(countNumberOfVisit)) {
+                showLooserOrWinner(true, userName);
+            } else {
+                showLooserOrWinner(false, userName);
+            }
             setCountNumberOfVisit(countNumberOfVisit + 1);
-            if (surpriseBoxwinningNumber === countNumberOfVisit) {
+            setNumberOfUsersVisted([
+                ...numberOfUsersVisted,
+                { userName: userName, userCount: countNumberOfVisit },
+            ]);
+        }
+        function showLooserOrWinner(status, userName) {
+            if (status) {
                 setMessage({
                     messageColour: "winner",
-                    showMessage: `hii ${userName} you are winnner`,
+                    showMessage: `Congratulations ${userName} you are winnner`,
                 });
             } else {
                 setMessage({
                     messageColour: "looser",
-                    showMessage: `hii ${userName} you are looser`,
+                    showMessage: `Hi ${userName} you are looser`,
                 });
             }
-        } else {
-            setMessage({
-                messageColour: "visitsecondtime",
-                showMessage: "you can't visit second time",
-            });
         }
     };
 
@@ -40,7 +62,6 @@ const SurpriseBox = () => {
                 value={userName}
                 onChange={(e) => setUserName(e.target.value)}
             />
-
             <button onClick={() => SubmituserName()}>click</button>
             <div className={message.messageColour}>
                 <b>{message.showMessage}</b>
